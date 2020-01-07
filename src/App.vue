443a78541224a9f39ff6a2a929e3480eab11a3f0
Jason <jason.lloyd.law@gmail.com>
2020-01-07 20:58:59 +1000

    moved loan calc data from component to state added steps for calculator fields moved app styles to root app.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-  <div id="app">
+  <div class="min-h-screen text-gray-600 bg-gray-200" id="app">
     <div id="nav" class="bg-gray-300">
       <div class="container mx-auto flex justify-between">
         <div>BM Toolbox</div>
-        <div>
+        <div class="font-semibold">
           <router-link to="/">Home</router-link> | 
           <router-link to="/about">About</router-link>
-        </div>
-        <div>
-          Pro Version
         </div>
       </div>
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,38 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loan_calculator: {
+      vehicle_price: 30000,
+      accessories: 0,
+      trade_value: 0,
+      trade_payout: 0,
+      payout_lender: "",
+      sign_deposit: 2000,
+      further_deposit: 0,
+      origination_fee: 975,
+      brokerage: 0,
+      gap: 0,
+      cci: 0,
+      warranty: 0,
+      lender_fee: 398,
+      other_fee: 0,
+      monthly_fee: 0,
+      term: 48,
+      apr: 5.99,
+      rv: 0,
+      rv_percent: 0,
+      weekly_big: true,
+      fort_big: false,
+      monthly_big: false,
+      hidden: false
+    },
+    saved_quotes_array: []
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {}
+});
