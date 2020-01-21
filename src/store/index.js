53de397177992cabf2bfc16d47159fdcd0c896eb
Jason Law <jason.lloyd.law@gmail.com>
2020-01-22 09:33:42 +1000

    api url changed to live server

--- src/store/index.js
+++ src/store/index.js
@@ -258,10 +258,10 @@ export default new Vuex.Store({
 		},
 		unsaved_changes: false,
 		app_import_field: '',
-		// visitor_log_url: 'https://api.steamdiesel.dev/api/lite-bdfi-app/visitor-log',
-		// details_update_url: 'https://api.steamdiesel.dev/api/lite-bdfi-app/user-details-update',
-		visitor_log_url: 'http://backend.test/api/lite-bdfi-app/visitor-log',
-		details_update_url: 'http://backend.test/api/lite-bdfi-app/user-details-update',
+		visitor_log_url: 'https://api.steamdiesel.dev/api/lite-bdfi-app/visitor-log',
+		details_update_url: 'https://api.steamdiesel.dev/api/lite-bdfi-app/user-details-update',
+		// visitor_log_url: 'http://backend.test/api/lite-bdfi-app/visitor-log',
+		// details_update_url: 'http://backend.test/api/lite-bdfi-app/user-details-update',
 	},
 	getters: {
 		application: state => {
