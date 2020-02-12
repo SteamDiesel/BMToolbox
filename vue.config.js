17941b0ec930c55e8efd836bfde786e523b85052
Jason Law <jason.lloyd.law@gmail.com>
2020-02-12 21:43:48 +1000

    removed pwa options from vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
 	devServer: {
 		host: 'lite.bdfi.test'
-	},
-	pwa: {
-		name: 'BDFI',
-		themeColor: '#2d3748',
-		msTileColor: '#2d3748',
-		appleMobileWebAppCapable: 'no',
-		appleMobileWebAppStatusBarStyle: 'default',
-		manifestPath: 'manifest.json',
-		workboxPluginMode: 'GenerateSW',
+	// },
+	// pwa: {
+	// 	name: 'BDFI',
+	// 	themeColor: '#2d3748',
+	// 	msTileColor: '#2d3748',
+	// 	appleMobileWebAppCapable: 'no',
+	// 	appleMobileWebAppStatusBarStyle: 'default',
+	// 	manifestPath: 'manifest.json',
+	// 	workboxPluginMode: 'GenerateSW',
 	}
 }
