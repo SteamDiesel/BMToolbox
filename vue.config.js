273889b4388d4f3e512eeda42be4fd94f93cf5fe
Jason Law <jason.lloyd.law@gmail.com>
2020-02-13 09:33:53 +1000

    changed service worker to manual settings to try to overcome deploy bug

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,21 @@
 module.exports = {
 	devServer: {
 		host: 'lite.bdfi.test'
-	// },
-	// pwa: {
-	// 	name: 'BDFI',
-	// 	themeColor: '#2d3748',
-	// 	msTileColor: '#2d3748',
-	// 	appleMobileWebAppCapable: 'no',
-	// 	appleMobileWebAppStatusBarStyle: 'default',
-	// 	manifestPath: 'manifest.json',
-	// 	workboxPluginMode: 'GenerateSW',
+	},
+	pwa: {
+		name: 'BDFI',
+		themeColor: '#2d3748',
+		msTileColor: '#2d3748',
+		appleMobileWebAppCapable: 'no',
+		appleMobileWebAppStatusBarStyle: 'default',
+		manifestPath: 'manifest.json',
+		workboxPluginMode: 'InjectManifest',
+		
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: 'sw.js',
+			// ...other Workbox options...
+			}
+		
 	}
 }
