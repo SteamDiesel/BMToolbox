ffb34d5a4c39b219165c35127f2f6ddb5e51a1df
Jason Law <jason.lloyd.law@gmail.com>
2020-02-13 11:26:45 +1000

    added exclude for service worker for _redirects

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,6 +20,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox
 workbox.core.setCacheNameDetails({prefix: "litebdfiapp"});
 
 self.addEventListener('message', (event) => {
+	
+    if ( event.request.url.match( '^.*(\/_redirects\/).*$' ) ) {
+        return false;
+    }
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
