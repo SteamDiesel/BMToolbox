51fc01c13e1a11287272a1ecf62e93dbbf47d703
Jason Law <jason.lloyd.law@gmail.com>
2020-02-29 13:19:45 +1000

    hidden redundant buttons on mobile in applications page

--- src/views/Applications.vue
+++ src/views/Applications.vue
@@ -1,18 +1,21 @@
 <template>
 	<div class="pb-12">
 		<PageHeader	 title="Applications">
-			<button @click="is_table = true" :class="{'bg-gray-500':is_table}" class=" h-8 rounded-l-lg bg-gray-200 border border-gray-600 text-xs py-1 px-2 ">
-				Table
-			</button>
-			<button @click="is_table = false" :class="{'bg-gray-500':!is_table}" class=" h-8 rounded-r-lg bg-gray-200 border border-gray-600 text-xs py-1 px-2  mr-4">
-				Board
-			</button>
-			<button
-				@click="show_import = !show_import"
-				class="bg-transparent font-semibold hover:bg-teal-200 px-4 py-2 border border-teal-600 rounded mr-4"
-			>
-			Import Application
-			</button>
+			<div class="hidden md:block">
+				<button @click="is_table = true" :class="{'bg-gray-500':is_table}" class="h-8 rounded-l-lg bg-gray-200 border border-gray-600 text-xs py-1 px-2 ">
+					Table
+				</button>
+				<button @click="is_table = false" :class="{'bg-gray-500':!is_table}" class="h-8 rounded-r-lg bg-gray-200 border border-gray-600 text-xs py-1 px-2  mr-4">
+					Board
+				</button>
+				<button
+					@click="show_import = !show_import"
+					class="bg-transparent font-semibold hover:bg-teal-200 px-4 py-2 border border-teal-600 rounded mr-4"
+				>
+				Import Application
+				</button>
+			</div>
+			
 			<NewApplicationButton />
 		</PageHeader>
 		
