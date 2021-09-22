4f44c88c4814e51535a851455371b38d236abe2d
SteamDiesel <jason.lloyd.law@gmail.com>
2021-09-22 23:03:53 +1000

    updated about description page to reflect offline frist update updated data dump page copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,53 +2,84 @@
 	<div class="pt-12 pb-24 container mx-auto px-4 sm:px-2">
 		<div>
 			<h1 class="text-2xl">About lite.BDFI.app</h1>
-			<p
-				class="mt-5"
-			>Brokerage & Dealership Finance & Insurance.</p>
-
+			<p class="mt-5">Brokerage & Dealership Finance & Insurance.</p>
 		</div>
 		<div class="mt-16">
-			<h1 class="text-2xl">Offline first</h1>
+			<h1 class="text-2xl">Offline-first</h1>
 			<p class="mt-5">
-				BDFI is developed to be an "offline-first" web application. This means that it doesn't depend on an internet connection to function.
+				BDFI is an "offline-first" web application. This means that it
+				doesn't depend on an internet connection to function.
 			</p>
 			<p class="mt-5">
-				Why offline-first? Unreliable internet connection strikes at the worst times, often when you're sitting with a client ready to take their application... 
-				<br>If this happens, you won't be stuck making desperate small talk while you wait for the connection to improve. This application with all of your client data can be used without any connection at all.
+				Why offline-first? Unreliable internet connection strikes at the
+				worst times, often when you're sitting with a client ready to
+				take their application...
+				<br />If this happens, you won't be stuck making desperate small
+				talk while you wait for the connection to improve. Every feature
+				of this application, including access to all of your client
+				data, can be used without any connection at all!
 			</p>
 			<p class="mt-5">
-				You can even install this application quickly and easily on your desktop or mobile device. So check your browser address bar for an icon inviting you to install it.
+				You can even install this application quickly and easily on your
+				desktop or mobile device. So check your browser address bar for
+				an icon inviting you to install it.
 			</p>
-			
 		</div>
 		<div class="mt-16">
 			<h1 class="text-2xl">Where is your data?</h1>
 			<p class="mt-5">
-				All data you enter into lite.bdfi.app is saved to your browsers local storage (on your computer, tablet or mobile) and all client applications are stored as fully encrypted entries in our secure cloud infrastructure. <br>
-				This "offline-first" approach gives you the benefits of additional data security and persistent offline access.
+				All data you enter into lite.bdfi.app is saved to your browsers
+				local storage (on your computer, tablet or mobile). <br />
+				This offline-first approach gives you the benefits of additional
+				data security and persistent offline access.
 			</p>
 			<p class="mt-5">
-				You are also able to retrieve all of your application data easily, if you wish to also keep your own offline storage archive.
-				<router-link class="hover:underline text-indigo-500 font-semibold" to="/data-dump">
+				You are also able to retrieve all of your application data
+				easily, if you wish to also keep your own offline storage
+				archive.
+				<router-link
+					class="hover:underline text-indigo-500 font-semibold"
+					to="/data-dump"
+				>
 					See all of your data here.
-				</router-link> 
+				</router-link>
 			</p>
 		</div>
 		<div class="mt-16">
-			<h1 class="text-2xl">Is my client data secure?</h1>
+			<h1 class="text-2xl">Could I lose my data?</h1>
 			<p class="mt-5">
-				Yes, we take data security and protection very seriously. <br> 
-				All client data stored in our secure cloud is fully encrypted using AES-256.
+				Yes, there is a chance that if your computer fails, then all of
+				your data is at risk. <br />
+				You should take regular backups of your data and store them in a
+				usb key or cloud storage of your choice.
 			</p>
 			<p class="mt-5">
-				Your connection with our secure cloud is via an encrypted ssl connection, with no client information accessible to packet sniffing attacks.
+				<router-link
+					class="hover:underline text-indigo-500 font-semibold"
+					to="/data-dump"
+				>
+					See all of your data here.
+				</router-link>
+			</p>
+		</div>
+		<div class="mt-16">
+			<h1 class="text-2xl">Can BDFI back up my data for me?</h1>
+			<p class="mt-5">
+				Yes, BDFI will offer automated secure cloud backups. This is
+				part of a set of premium features that will be available in the
+				future.
+			</p>
+		</div>
+		<div class="mt-16">
+			<h1 class="text-2xl">Is my locally stored data secure?</h1>
+			<p class="mt-5">
+				Yes, we take data security and protection very seriously. <br />
+				The data stored on your device is not accessible to the
+				internet.
 			</p>
 			<p class="mt-5">
-				This means that the only security vulnerability to consider from here... is you (and anyone who has access to your devices and account). 
-				<br> Please ensure you practice good online security by picking a long and unique password not shared with other accounts you have. 
-			</p>
-			<p class="mt-5">
-				Better still, use a password manager to generate and store your passwords for you. We use and recommend <a class="hover:underline text-indigo-500 font-semibold" href="https://www.lastpass.com/" target="_blank">LastPass</a>.
+				Your connection to our secure cloud is via an encrypted
+				connection, with no client information accessible.
 			</p>
 		</div>
 	</div>
--- src/views/DataDump.vue
+++ src/views/DataDump.vue
@@ -4,44 +4,45 @@
 			<h1 class="text-2xl">Storing your data</h1>
 			<p class="mt-5">
 				The easy ways to backup this data:
-				<br />- Copy all of the text below, and save it into a new text document on your computer.
-				<br />- Print this page to a pdf (You could print it to paper, but then you won't be able to re-import it to this system.)
-				<br /> <br>
-				With your data retrieved, you could keep frequent backups as a point-in-time data 
-				backup. From here you can upload it to your own cloud storage or print it to paper 
-				for long term air-gapped storage. <br>
-				Also, if you're really adventurous, you could use the data in your own tools. 
+				<br />1. Copy all of the text below and save it into a new text
+				document OR print this page to a pdf. <br />2. Store that file
+				on a usb stick or upload it to a cloud storage of your
+				choice.<br />
 			</p>
 		</div>
 		<div class="mb-24 break-after">
 			<h1 class="text-2xl">User Preferences:</h1>
 			<p class="text-xs">
-				{{user_preferences}}
+				{{ user_preferences }}
 			</p>
-			
 		</div>
 		<div class="mb-24">
 			<h1 class="text-2xl">Application Data:</h1>
-			<div v-for="(app, index) in applications" :key="index" class="break-after mb-16">
+			<div
+				v-for="(app, index) in applications"
+				:key="index"
+				class="break-after mb-16"
+			>
 				<h2>
-					<span v-for="(p, index) in app.people" :key="index">{{p.first_name}} {{p.surname}} </span> <br>
-
+					<span v-for="(p, index) in app.people" :key="index"
+						>{{ p.first_name }} {{ p.surname }}
+					</span>
+					<br />
 				</h2>
 				<p class="text-xs">
-					{{app}}
+					{{ app }}
 				</p>
 			</div>
-			
 		</div>
 	</div>
 </template>
 <script>
-// @ is an alias to /src
-import { mapState } from "vuex";
-export default {
-	name: "DataDump",
-	computed: {
-		...mapState(["applications", "user_preferences"])
-	}
-};
+	// @ is an alias to /src
+	import { mapState } from "vuex";
+	export default {
+		name: "DataDump",
+		computed: {
+			...mapState(["applications", "user_preferences"]),
+		},
+	};
 </script>
